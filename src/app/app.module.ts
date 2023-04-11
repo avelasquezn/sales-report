13ce5c76930d0204017d16ab190fb1d50e6586e2
Angel Velasquez <pcisavel@upc.edu.pe>
2023-04-11 09:17:52 -0500

    feat: added sales report behavior.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {DatePipe} from "@angular/common";
 import { ItemListComponent } from './sales/item-list/item-list.component';
+import { ReportComponent } from './sales/report/report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemListComponent
+    ItemListComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule
